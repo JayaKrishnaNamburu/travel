196a4047ca2884faf1bb5e945e0c74b9b9409f5f
JayaKrishna <namburu1995@gmail.com>
2018-12-21 15:33:36 +0530

    Added gh-pages for deployment

---
+++ src/components/Holder/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+
+const Holder = () => {
+    return (
+        <div
+            className="holder"
+        >
+            This is holder
+        </div>
+    );
+}
+
+export default Holder
