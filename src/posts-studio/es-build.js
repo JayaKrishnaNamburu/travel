4c9f394eff9163ef48c420ead603a2da22823e03
JayaKrishnaNamburu <namburu1995@gmail.com>
2021-06-11 16:59:51 +0530

    Fixing typos and cleaning up notes

--- src/posts-studio/es-build.js
+++ src/posts-studio/es-build.js
@@ -36,22 +36,22 @@ const EsBuild = () => {
           <h1 className={styles.text003}> Monorepo&apos;s</h1>
         </div>
         <p className={styles.text004}>
-          <span>The state of Javascript development</span>
+          <span>The state of Javascript developer</span>
           <span className={styles.text006}> tooling</span>
           <span>
             {' '}
-            is exiting and moving at much faster pace than ever. JavaScript being a high level and
+            is exciting and moving at much faster pace than ever. JavaScript being a high level and
             dynamic language, we end up coming across many runtime errors than a traditional
             compiler based languages (i.e rust, go, c++). It has it&apos;s own
           </span>
           <span className={styles.text008}>pros</span>
           <span> and</span>
-          <span className={styles.text010}>cons</span>
+          <span className={styles.text010}>{' '}cons</span>
           <span> at the same time.</span>
           <br></br>
           <br></br>
           <span>
-            One major advantage is, the learning curve is little easier because, the feedback loop
+            One major advantage is, learning curve is little easier as the feedback loop
             of seeing our projects running is quick. But the same will
           </span>
           <span className={styles.text016}> bite</span>
@@ -68,10 +68,10 @@ const EsBuild = () => {
           <span className={styles.text026}> prettier</span>
           <span>
             {' '}
-            and many other build time and development time targeting tools. Has mad the life of
+            and many other build time and development time targeting tools. These JS tooling has made our lifes as
             developers much more easier and at the same time reducing the run-time errors that we
             often run into. But, these tools being again written in JS, starts to show the short
-            comes when the applications starts to grow bigger and bigger.
+            comings when the applications starts to grow bigger.
           </span>
         </p>
         <p className={styles.text028}>
@@ -82,15 +82,13 @@ const EsBuild = () => {
             about the slowness of build tools, but what actually got me thinking into all this ?
           </span>
           <br></br>
-          <span>Why should i bother, moving my build pipelines to use</span>
-          <span className={styles.text034}> esbuild</span>
-          <span> ?</span>
+          <span>Why should i bother, moving my build pipelines which is absolutely stable at the moment ?</span>
           <br></br>
           <span className={styles.text038}></span>
           <br></br>
-          <span>There is a hight chance of you listening about</span>
-          <span className={styles.text041}> esbuild</span>
-          <span> by now, I could direct you to some of the blogposts that talk about </span>
+          <span>There is a high chance of you coming across</span>
+          <span className={styles.text041}>{' '}esbuild</span>
+          <span>{' ' }I could direct you to some of the blogposts that talk about </span>
           <a href="https://github.com/evanw/esbuild" target="_blank" rel="noreferrer noopener">
             <span>esbuild</span>
           </a>
@@ -116,12 +114,12 @@ const EsBuild = () => {
             target="_blank"
             rel="noreferrer noopener"
           >
-            <span>monorepo</span>
+            <span>{' '}monorepo</span>
           </a>
-          <span>. As, the project started growing, we started running into</span>
-          <span className={styles.text053}> delays</span>
+          <span>{' '} of code generators. As, the project started to grow, we started running into</span>
+          <span className={styles.text053}> delayed build times.</span>
           <span>. The build times are taking a</span>
-          <span className={styles.text055}>massive</span>
+          <span className={styles.text055}>{' '}massive</span>
           <span>
             {' '}
             hit. Our monorepo, consists of 60+ packages to support all frameworks that we target at
@@ -141,27 +139,25 @@ const EsBuild = () => {
           <span>You can see, on an average the build time is minimum</span>
           <span className={styles.text065}> 10mins</span>
           <span>. And this started to show effect in local too, even when we had a</span>
-          <span className={styles.text067}>custom-watcher.</span>
+          <span className={styles.text067}>{' '}custom-watcher.{' ' }</span>
           <span>
-            That takes care of watching the package that just changes and build it, which you can
+            That takes care of watching the package and rebuilding them, which you can
             find here. But still the initial startup and build time at end is consuming a
             considerable time.
           </span>
-          <span className={styles.text070}>
             <br />
-          </span>
         </p>
         <p className={styles.text071}>
           <span>
-            We don&apos;t use Webpack or rollup build tools. Since, we just need to transpile the
+            We don&apos;t use Webpack or rollup build tools. We just need to transpile the
             code to
           </span>
           <span className={styles.text073}> es6</span>
           <span>
             {' '}
-            and publish to npm. Any bundling, transpiling it more will handled by the users
+            and publish to npm. Any bundling with dependencies, transpiling will handled by the users
             consuming the library. That&apos;s the best way to go, or else we might end up bundling
-            two times. First when we do it before publishing to npm and once again when the users
+            two times, transpiling towo times. First when we do it before publishing to npm and once again when the users
             using in their build systems.
           </span>
         </p>
@@ -208,34 +204,40 @@ const EsBuild = () => {
           <span>I started to put together some custom scripts to achieve this.</span>
           <br></br>
           <span>A custom</span>
-          <span className={styles.text104}>build</span>
+          <span className={styles.text104}>{' ' }build</span>
           <span>
             {' '}
-            script that walks all the packages at depth as 1. And builds the package using
-            esbuild&apos;s node-api.
+            script that walks through all the packages at depth as 1. And builds the package using
+            esbuild&apos;s node-api. And the <span className={styles.text097}>package.json</span> of each package.
           </span>
         </p>
         <iframe
           title="custom-script"
-          src="data:text/html;charset=utf-8,     &lt;head&gt;&lt;base target='_blank' /&gt;&lt;/head&gt;     &lt;body&gt;&lt;script src=&quot;https://gist.github.com/JayaKrishnaNamburu/77582aa77b2ff623bbcfd0ae61e6d8dc.js&quot;&gt;&lt;/script&gt;     &lt;/body&gt;"
+          src={`
+          data:text/html;charset=utf-8,     <head>
+          <base target='_blank' />
+          </head>     
+          <body>
+          
+          <script src="https://gist.github.com/JayaKrishnaNamburu/77582aa77b2ff623bbcfd0ae61e6d8dc.js"></script>
+          </body>`}
           className={styles.iframe}
         ></iframe>
         <p className={styles.text106}>
-          A Custom watcher to look for packages, that are going under change and rebuilding them
-          again.
+          A Custom watcher to look for changes in packages, and rebuilding them.
         </p>
         <iframe
           title="custom-watcher"
           src="data:text/html;charset=utf-8,     &lt;head&gt;&lt;base target='_blank' /&gt;&lt;/head&gt;     &lt;body&gt;&lt;script src=&quot;https://gist.github.com/JayaKrishnaNamburu/ebf0b4bb7753affb0b4db9bab013882c.js&quot;&gt;&lt;/script&gt;     &lt;/body&gt;"
           className={styles.iframe1}
         ></iframe>
-        <span className={styles.text107}>
+        <p className={styles.text107}>
           <span>and now configuring</span>
           <span className={styles.text109}>npm scripts</span>
           <span> to run</span>
-          <span className={styles.text111}>tsc</span>
-          <span> to generate types alone.</span>
-        </span>
+          <span className={styles.text111}>{' '}typescript compiler</span>
+          <span> to generate types and a static type checking.</span>
+        </p>
         <iframe
           title="npm-scripts"
           src="data:text/html;charset=utf-8,     &lt;head&gt;&lt;base target='_blank' /&gt;&lt;/head&gt;     &lt;body&gt;&lt;script src=&quot;https://gist.github.com/JayaKrishnaNamburu/ffae981886b721db9c87a5ba88325503.js&quot;&gt;&lt;/script&gt;     &lt;/body&gt;"
@@ -243,6 +245,7 @@ const EsBuild = () => {
         ></iframe>
         <p className={styles.text113}>
           <span>And here is the final result.</span>
+          <br />
           <br></br>
           <span>
             - Packages transpiled using esbuild. And types are generated in parallel using lerna.
@@ -259,7 +262,9 @@ const EsBuild = () => {
             lower syntax. The user&apos;s build systems can do that again.
           </span>
           <br></br>
-          <span>Ok, let&apos;s talk about the results,</span>
+          <span>Ok, let&apos;s talk about the results. It's clear that we are having an average of 
+          <span className={styles.text057}>{' ' }5~6minutes{' '}</span>
+            in our build time now.</span>
         </p>
         <img
           alt="placeholder"
