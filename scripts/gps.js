d670a591aa0dfc58bee082fdd868c6de809a7adc
Jaya Krishna Namburu <namburu1995@gmail.com>
2022-06-07 10:56:14 +0000

    Adding new stuff

--- scripts/gps.js
+++ scripts/gps.js
@@ -1,53 +1,78 @@
-const { ExifImage } = require('exif')
-const walk = require('walkdir')
-const { dirname, basename, join, extname } = require('path')
-const places = require('../src/components/Map/points.json')
-const { writeFileSync } = require('fs')
+const { ExifImage } = require("exif");
+const walk = require("walkdir");
+const { dirname, basename, join, extname } = require("path");
+const places = require("../src/components/Map/points.json");
+const { writeFileSync } = require("fs");
 
-const capitalize = (str) => str[0].toUpperCase() + str.slice(1)
-const newPlaces = []
-const promises = []
-const emitter =  walk('src/images', (path, stat) => {
-        if (!extname(path)) {
-            return
-        }
-        
-        promises.push(
-           new Promise((resolve, reject) => {
-               try {
-                new ExifImage({ image: path }, (err, meta) => {
-                    if (err) {
-                        return reject(err)
-                    }
-                    const location = capitalize(basename(dirname(path)))
-                    if (!meta?.gps) {
-                        return reject(`No location data`)
-                    }
-        
-                    const { GPSLatitude, GPSLongitude  } = meta.gps
-                    if (!GPSLatitude || !GPSLongitude) {
-                        return reject('No GPS lat-long data')
-                    }
-                    
-                    console.log(GPSLatitude, GPSLongitude)
-                    newPlaces.push({ name: location, points: [GPSLatitude, GPSLongitude] })
-                    return resolve()
-                })
-               } catch(e) {
-                   console.log(e)
-               }
-           })
-        )
-})
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+const convertDMSToDD = (degrees, minutes, seconds, direction) => {
+  let dd = +degrees + +minutes / 60 + +seconds / (60 * 60);
+  if (direction === "S" || direction === "W") {
+    dd = dd * -1;
+  }
+  return dd;
+};
 
-emitter.on('end', async () => {
-    try {
-        await Promise.allSettled(promises)
-        console.log(newPlaces)
-        // writeFileSync(join(__dirname, '../src/components/Map/points.json'), JSON.stringify([...places, ...newPlaces], null, 2), 'utf-8')
-        // console.log('Parsing GPS coordinates done !')
-    } catch(e) {
-        console.log(e)
-    }
-})
+const newPlaces = [];
+const promises = [];
+const emitter = walk("src/images", (path, stat) => {
+  if (!extname(path)) {
+    return;
+  }
 
+  promises.push(
+    new Promise((resolve, reject) => {
+      try {
+        new ExifImage({ image: path }, (err, meta) => {
+          if (err) {
+            return reject(err);
+          }
+          const location = capitalize(basename(dirname(path)));
+          if (!meta?.gps) {
+            return reject(`No location data`);
+          }
+
+          const {
+            GPSLatitude,
+            GPSLongitude,
+            GPSLatitudeRef,
+            GPSLongitudeRef,
+          } = meta.gps;
+          if (
+            !GPSLatitude ||
+            !GPSLongitude ||
+            !GPSLatitudeRef ||
+            !GPSLongitudeRef
+          ) {
+            return reject("No GPS lat-long data");
+          }
+
+          newPlaces.push({
+            name: location,
+            points: [
+              convertDMSToDD(...GPSLongitude, GPSLongitudeRef),
+              convertDMSToDD(...GPSLatitude, GPSLatitudeRef),
+            ],
+          });
+          return resolve();
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    })
+  );
+});
+
+emitter.on("end", async () => {
+  try {
+    await Promise.allSettled(promises);
+    writeFileSync(
+      join(__dirname, "../src/components/Map/points.json"),
+      JSON.stringify([...places, ...newPlaces], null, 2),
+      "utf-8"
+    );
+    console.log("Parsing GPS coordinates done !");
+  } catch (e) {
+    console.log(e);
+  }
+});
--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -1,8 +1,6 @@
 export const images = {
-  varkala: [1, 2, 3, 4],
-  rishikesh: [1],
   kedarkantha: [1, 2, 3, 4],
-  banglore: [1, 4],
+  banglore: [4],
   udaipur: [1, 2],
   yercaud: [1, 2],
   wayanad: [1, 2],
@@ -10,8 +8,14 @@ export const images = {
   coorg: [1, 2, 3],
   pollachi: [1],
   pichavaram: [1, 2],
-  vizag: [1,2],
+  vizag: [1, 2],
   brahmatal: [2, 3],
-  kanyakumari: [1]
-}
-
+  kanyakumari: [1],
+  dandeli: [1, 2],
+  'jog-falls': [1],
+  'kundle-beach': [1],
+  panjim: [1, 3],
+  ponda: [1],
+  varkala: [1, 2, 3, 4],
+  rishikesh: [1]
+};
